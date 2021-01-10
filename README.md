75315394169163575e49e13638f44e15ea963ca1
Balinca Gabriel Alin <balinca.gabriel.alin@gmail.com>
2021-01-10 15:56:05 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,61 @@
+# Counter-Strike 1.6 Server Script
+* This is a script for automating a counter-strike 1.6 server installation.
+
+# Instllation
+* Starting the installation its simple just run this command :
+
+```
+./compile.sh
+```
+
+* The script will ask you for server name and port.
+
+# Modifications and plugins used
+
+| Mod           | Vserion |
+|---------------|:-------:|
+| rehlds        | 3.7.0   |
+| amxmodx       | 1.8.2   |
+| metamod-r     | 1.3.128 |
+| rechecker     | 2.5     |
+| cstrike       | 1.8.2   |
+| ReGameDLL     | 5.3.0   |
+| Reunion       | 0.1.92  |
+| ReAimDetector | 0.2.2   |
+| ReAuthChecker | 0.1.6   |
+
+# Control
+* The script will make the server run in the background with the help of screen utility.
+* If you want to learn to use this utility follow this link :
+
+  https://ixnfo.com/en/how-to-install-and-use-screen.html
+
+# Links
+* reHLDS :  
+https://github.com/dreamstalker/rehlds  
+http://nexus.rehlds.org/nexus/content/repositories/rehlds-dev/rehlds/rehlds/3.7.0.693-dev/
+
+
+* Metamod-r :  
+https://github.com/theAsmodai/metamod-r
+
+* AmxModX :  
+https://www.amxmodx.org/downloads.php  
+https://www.amxmodx.org/release/amxmodx-1.8.2-base-linux.tar.gz  
+https://www.amxmodx.org/release/amxmodx-1.8.2-cstrike-linux.tar.gz
+
+* ReChecker :  
+https://www.dedicated-server.ru/vbb/showthread.php?t=26550
+
+* cstrike fix and ReGameDLL for reHLDS :  
+https://github.com/s1lentq/ReGameDLL_CS/
+https://c-s.net.ua/forum/topic81470.html#entry917762
+
+* Reunion :  
+http://repository.btstelecom.ro/resources/reunion_0.1.92.zip
+
+* ReAimDetector :  
+http://www.dedicated-server.ru/vbb/showthread.php?t=27162
+
+* ReAuthChecker :  
+https://c-s.net.ua/forum/topic89180.html
---
+++ compile.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# Read server name
+read -p "Server Name : " input_server_name 
+
+# Lowercase the input
+server_name=${input_server_name,,}
+
+# Check if the folder already exist
+find . -type d | grep $server_name 
+
+until [ $? -ne 0 ]
+do
+	echo "This name already is in use ! Please choose another name"
+	read -p "Server Name : " input_server_name 
+	server_name=${input_server_name,,}
+done
+
+# Read the port number
+read -p "Port Number : " port
+
+# Support for 32-bit architecture
+dpkg --add-architecture i386
+
+# Updating package list
+apt-get update -y
+
+# Install required packages
+apt-get install -y libsdl2-2.0-0 libsdl2-2.0-0:i386 lib32gcc1 tmux screen curl
+
+# Downloading steamcmd
+mkdir /home/resources/steamcmd &&\
+cd /home/resources/steamcmd &&\
+curl -sqL "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar zxvf -
+if [ $? -ne 0 ]; then
+	cd /
+	echo "ERROR : Cannot download steamcmd !"
+else
+	cd /
+fi
+
+# Installing the server
+until /home/resources/steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/$server_name +app_update 90 -beta beta validate +quit; do sleep 1; done
+
+# Fixing steamclient.so issue
+rm -rf /root/.steam/sdk32
+mkdir /root/.steam/sdk32
+cp /home/$server_name/steamclient.so /root/.steam/sdk32
+
+# Non-steam patch
+cp -r /home/resources/nonsteam-patch/* /home/$server_name
+
+# Installing base mods (cstrike fix, regamedll, metamod, amxmodx, reunion, rechecker, reauthchecker, reaimdetector)
+cp -r /home/resources/cstrike/* /home/$server_name/cstrike
+
+# Change server name
+sed -i 's,hostname "Counter-Strike 1.6 Server",hostname "'"$input_server_name"'",g' /home/$server_name/cstrike/server.cfg
+
+# Script for starting the server
+cd /home/$server_name
+touch start.sh
+echo -n "screen -A -m -d -S $server_name ./hlds_run -game cstrike +maxplayers 25 +map de_dust2 -port " >> start.sh
+echo -n $port >> start.sh
+echo -n " -autoupdate -pingboost 3" >> start.sh
+chmod +x start.sh
+./start.sh
+
+# Aditional cfgs
+mkdir /home/$server_name/cstrike/banned.cfg
+mkdir /home/$server_name/cstrike/listip.cfg
